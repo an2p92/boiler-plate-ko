7f4a11012aa2fe02fb5d9ce5dcd82f60bf23c9e7
an2p92 <an2p92@wooriict.com>
2022-04-04 16:48:00 +0900

    포스트맨 으로 post 통신

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require('express');
+const app = express();
+const port = 5000;
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
 
+//application/x-www-form-urlencode
+app.use(bodyParser.urlencoded({extended: true}));
 
-const mongoose = require('mongoose')
+//application/json
+app.use(bodyParser.json());
+
+const mongoose = require('mongoose');
 
 mongoose.connect('mongodb+srv://an2p92:123@boilerplate.dsxjp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
     // useNewUrlParser: true, 
@@ -11,8 +18,31 @@ mongoose.connect('mongodb+srv://an2p92:123@boilerplate.dsxjp.mongodb.net/myFirst
     // useCreateIndex: true, 
     // useFindAnmodify: false
 }).then(() => console.log('MongoDB Connected..dd.'))
- .catch(err => console.log(err))
+ .catch(err => console.log(err));
 
-app.get('/', (req, res) => {res.send('Hello World!~~~~~~안녕하세요')})
+
+
+
+
+app.get('/', (req, res) => res.send('Hello World!~~~~~~안녕하세요'));
+
+app.post('/register', (req, res) => {
+
+    //회원 가입 할때 필요한 정보들을 client에서 가져오면
+    //그것들을 데이터 베이스에 넣어준다.
+
+    const user = new User(req.body)
+    user.save((err, userInfo) => {
+        if(err) return res.json({ succes: false, err})
+        return res.status(200).json({
+            succes: true
+        })
+
+    })
+
+});
+
+
+
 
 app.listen(port, () => {console.log(`Example app listening on port ${port}`)})
