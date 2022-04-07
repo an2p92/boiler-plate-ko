da857e7190c1ffbca8e5c502bf191fca714c4d85
an2p92 <an2p92@wooriict.com>
2022-04-07 16:03:38 +0900

    로그인기능 토큰 기능 추가

--- models/User.js
+++ models/User.js
@@ -1,7 +1,10 @@
 
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10//암호화할 자리수???
+const jwt = require('jsonwebtoken');
 
-const userSchema = mongoose.Schema({ 
+const userSchema = mongoose.Schema({
     name:   {
         type: String,
         maxlength: 50
@@ -13,7 +16,7 @@ const userSchema = mongoose.Schema({
     },
     password:  {
         type: String,
-        maxlength: 5
+        minlength: 5
     },
     lastname:  {
         type: String,
@@ -31,6 +34,47 @@ const userSchema = mongoose.Schema({
         type: Number
     },
 })
+
+userSchema.pre('save', function( next ) {
+    var user = this;
+    if(user.isModified('password')) {
+        //비밀번호를 암호화 시킨다
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            if(err) return next(err)
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                if(err) return next(err)
+                user.password = hash
+                next()
+            });
+        });        
+    } else {
+        next()
+    }
+
+
+})
+
+userSchema.methods.comparePassword = function(plainPassword, cb) {
+    //plainPassword 1234567 암호화된 비밀번호
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch) {
+        if(err) return cb(err);
+            cb(null, isMatch)
+    })
+}
+
+userSchema.methods.generateToken = function(cb) {
+    var user = this;
+    // jsonwebtoken 을 이용해서 token을 생성하기
+    var token = jwt.sign(user._id.toHexString(), 'secretToken')
+    // user._id + 'secretToken' = token
+    // ->
+    // 'secretToken' -> user._id
+    user.token = token
+    user.save(function(err, user) {
+        if(err) return cb(err)
+        cb(null, user)
+    })
+}
 
 const User =mongoose.model('User', userSchema)
 
